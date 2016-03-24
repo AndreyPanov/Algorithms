f10c5f4a5a1c863b1b15b7d70842def7256c53a6
Andrey Panov <panovdev@gmail.com>
2016-03-24 22:09:46 +0300

    remove comment

--- Playground/Trees.playground/Contents.swift
+++ Playground/Trees.playground/Contents.swift
@@ -39,21 +39,6 @@ class Tree {
         return max(countLevels(nodeUnwrapped.left), countLevels(nodeUnwrapped.right)) + 1
     }
 }
-/*
-Node n0, n1, n2, n3, n4, n5, n6, n7;
-
-n5 = new Node(5);
-n6 = new Node(6);
-n4 = new Node(4, n5, n6);
-n3 = new Node(3);
-n1 = new Node(1, n3, n4);
-
-n7 = new Node(7);
-n2 = new Node(2, n7, null);
-
-n0 = new Node(0, n1, n2);
-
-root = n0;*/
 
 let node5 = Node(value: 5)
 let node6 = Node(value: 6)
@@ -69,3 +54,4 @@ let node0 = Node(value: 0, left: node1, right: node2)
 let tree = Tree(root: node0)
 tree.countNodes(tree.root)
 tree.countLevels(tree.root)
+
