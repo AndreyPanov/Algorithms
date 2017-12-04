f86e9acf1481ddc173a5b97a913f69704feb47e6
Andrey Panov <panovdev@gmail.com>
2017-12-04 21:29:57 +0100

    New Swift project

---
+++ NewSwift/Algorithms/Algorithms/Problems/MergeSort.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class MergeSort {
+  
+  func sort(array: [Int]) -> [Int] {
+    
+  }
+  
+  private func merge(part: [Int]) -> [Int] {
+    
+  }
+}
---
+++ NewSwift/Algorithms/Algorithms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorithms
+//
+//  Created by Андрей on 04.12.17.
+//  Copyright © 2017 Andrey Panov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
