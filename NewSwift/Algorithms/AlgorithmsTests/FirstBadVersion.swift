9990c4c6cb80f18369b2308de6c408be16ebb1e9
Andrey Panov <panovdev@gmail.com>
2022-11-20 21:02:11 +0100

    Bad version update

--- NewSwift/Algorithms/AlgorithmsTests/FirstBadVersion.swift
+++ NewSwift/Algorithms/AlgorithmsTests/FirstBadVersion.swift
@@ -5,14 +5,29 @@ import XCTest
 final class FirstBadVersion: XCTestCase {
 
     func testExample() throws {
-        
+        XCTAssertEqual(1, firstBadVersion(1))
     }
     
     func firstBadVersion(_ n: Int) -> Int {
-        return 0
+        var left = 1
+        var right = n
+        var bad = -1
+        while left <= right {
+            let mid = (left + right)/2
+            if isBadVersion(mid) {
+                bad = mid
+                left = mid + 1
+            } else {
+                if bad != -1 {
+                    return bad
+                }
+                right = mid
+            }
+        }
+        return bad
     }
     
     func isBadVersion(_ version: Int) -> Bool{
-        return version == 4
+        return version <= 1
     }
 }
