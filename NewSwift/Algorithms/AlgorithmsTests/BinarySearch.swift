4eca4d305257a9331d818bafedd7081502ab67ee
Andrey Panov <panovdev@gmail.com>
2022-11-20 19:37:30 +0100

    Update to Swift 5

---
+++ NewSwift/Algorithms/AlgorithmsTests/BinarySearch.swift
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/binary-search
+import XCTest
+
+final class BinarySearch: XCTestCase {
+
+
+    func testExample() throws {
+        XCTAssertEqual(4, search([-1,0,3,5,9,12], 9))
+    }
+    
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        let count = nums.count
+        while true {
+            if 
+        }
+        
+        return 4
+    }
+
+}
