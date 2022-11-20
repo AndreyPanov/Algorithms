27b2f7338d996297cdf484864c761c4731664e0b
Andrey Panov <panovdev@gmail.com>
2022-11-20 20:24:33 +0100

    BinarySearch

--- NewSwift/Algorithms/AlgorithmsTests/BinarySearch.swift
+++ NewSwift/Algorithms/AlgorithmsTests/BinarySearch.swift
@@ -6,15 +6,24 @@ final class BinarySearch: XCTestCase {
 
     func testExample() throws {
         XCTAssertEqual(4, search([-1,0,3,5,9,12], 9))
+        //XCTAssertEqual(-1, search([-1,0,3,5,9,12], 2))
     }
     
     func search(_ nums: [Int], _ target: Int) -> Int {
-        let count = nums.count
-        while true {
-            if 
+        var left = 0
+        var right = nums.count-1
+        while left <= right {
+            let mid = (left + right)/2
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] > target {
+                right = mid-1
+            } else {
+                left = mid + 1
+            }
         }
         
-        return 4
+        return -1
     }
 
 }
---
+++ NewSwift/Algorithms/AlgorithmsTests/FirstBadVersion.swift
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/first-bad-version/
+
+import XCTest
+
+final class FirstBadVersion: XCTestCase {
+
+    func testExample() throws {
+        
+    }
+    
+    func firstBadVersion(_ n: Int) -> Int {
+        return 0
+    }
+    
+    func isBadVersion(_ version: Int) -> Bool{
+        return version == 4
+    }
+}
