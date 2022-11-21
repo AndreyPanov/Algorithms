014a0e7432b40669bc76c2462fee1a39764ac12d
Andrey Panov <panovdev@gmail.com>
2022-11-21 20:27:51 +0100

    SearchInsertPosition

---
+++ NewSwift/Algorithms/AlgorithmsTests/SearchInsertPosition.swift
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/search-insert-position/
+import XCTest
+
+final class SearchInsertPosition: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(2, searchInsert([1,3,5,6], 5))
+        //XCTAssertEqual(1, searchInsert([1,3,5,6], 2))
+        //XCTAssertEqual(4, searchInsert([1,3,5,6], 7))
+        
+    }
+    
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count-1
+        while left <= right {
+            let mid = (left + right)/2
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] > target {
+                right = mid-1
+            } else {
+                left = mid + 1
+            }
+        }
+        
+        return -1
+    }
+}
