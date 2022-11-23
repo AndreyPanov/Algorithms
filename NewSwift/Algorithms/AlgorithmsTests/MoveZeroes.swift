2b8786e7e19f5af403cc3bf5ca2f765f5823d433
Andrey Panov <panovdev@gmail.com>
2022-11-23 06:26:35 +0100

    Move Zeroes

---
+++ NewSwift/Algorithms/AlgorithmsTests/MoveZeroes.swift
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/move-zeroes
+import XCTest
+
+final class MoveZeroes: XCTestCase {
+
+    func testExample() throws {
+        
+    }
+    
+    func moveZeroes(_ nums: inout [Int]) {
+        
+    }
+}
