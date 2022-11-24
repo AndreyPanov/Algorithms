c90c78421dfa67108e4ae4dbf71cff6d52c720db
Andrey Panov <panovdev@gmail.com>
2022-11-24 20:52:01 +0100

    Two Sum II - Input Array Is Sorted

---
+++ NewSwift/Algorithms/AlgorithmsTests/TwoSumIIInputArrayIsSorted.swift
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted
+
+import XCTest
+
+final class TwoSumIIInputArrayIsSorted: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual([1,2], twoSum([2,7,11,15], 9))
+    }
+    
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        
+    }
+}
