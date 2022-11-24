16461248b45b4db4896f3328385eba80ef638b89
Andrey Panov <panovdev@gmail.com>
2022-11-24 20:48:07 +0100

    Move Zeros final

--- NewSwift/Algorithms/AlgorithmsTests/MoveZeroes.swift
+++ NewSwift/Algorithms/AlgorithmsTests/MoveZeroes.swift
@@ -4,10 +4,26 @@ import XCTest
 final class MoveZeroes: XCTestCase {
 
     func testExample() throws {
-        
+        var array = [0,1,0,3,12]
+        moveZeroes(&array)
+        XCTAssertTrue(true)
     }
     
     func moveZeroes(_ nums: inout [Int]) {
+        guard nums.count>1 else { return }
+            
+        var fast = 0
+        var slow = 0
         
+        while (fast < nums.count) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast]
+                slow += 1
+            }
+            fast += 1
+        }
+        for i in slow..<fast {
+            nums[i] = 0
+        }
     }
 }
