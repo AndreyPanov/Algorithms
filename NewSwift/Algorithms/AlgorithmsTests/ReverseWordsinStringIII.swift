86c3d06014b49d89cd956c5d955579b27ded6657
Andrey Panov <panovdev@gmail.com>
2022-11-25 20:36:03 +0100

    Reverse Words in a String III

---
+++ NewSwift/Algorithms/AlgorithmsTests/ReverseWordsinStringIII.swift
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+import XCTest
+
+final class ReverseWordsinStringIII: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual("s'teL ekat edoCteeL tsetnoc", reverseWords("Let's take LeetCode contest"))
+    }
+    
+    func reverseWords(_ s: String) -> String {
+        return ""
+    }
+}
