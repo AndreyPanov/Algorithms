edbf1e5544ac4666ae06163b621811068bafd0c3
Andrey Panov <panovdev@gmail.com>
2022-11-27 13:34:18 +0100

    Update LongestSubstringWithoutRepeatingCharacters.swift

--- NewSwift/Algorithms/AlgorithmsTests/LongestSubstringWithoutRepeatingCharacters.swift
+++ NewSwift/Algorithms/AlgorithmsTests/LongestSubstringWithoutRepeatingCharacters.swift
@@ -5,12 +5,30 @@ import XCTest
 final class LongestSubstringWithoutRepeatingCharacters: XCTestCase {
 
     func testExample() throws {
-        XCTAssertEqual(3, lengthOfLongestSubstring("abcabcbb"))
-        XCTAssertEqual(1, lengthOfLongestSubstring("bbbb"))
-        XCTAssertEqual(3, lengthOfLongestSubstring("pwwkew"))
+        //XCTAssertEqual(3, lengthOfLongestSubstring("abcabcbb"))
+        //XCTAssertEqual(1, lengthOfLongestSubstring("bbbb"))
+        //XCTAssertEqual(3, lengthOfLongestSubstring("pwwkew"))
+        XCTAssertEqual(1, lengthOfLongestSubstring(" "))
     }
 
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        return 0
+        let s = Array(s)
+        var maxLength = 0
+        var currentLength = 0
+        var storage = Set<Character>()
+        for i in 0..<s.count {
+            for j in i..<s.count {
+                currentLength = storage.count
+                storage.insert(s[j])
+                if storage.count > currentLength {
+                    currentLength = storage.count
+                } else {
+                    maxLength = max(maxLength, currentLength)
+                    storage.removeAll()
+                    break
+                }
+            }
+        }
+        return max(maxLength, currentLength)
     }
 }
