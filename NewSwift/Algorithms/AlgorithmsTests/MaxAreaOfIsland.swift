c255810a6be0557971e4bf766f6ed7d21dd2d81c
Andrey Panov <panovdev@gmail.com>
2022-12-03 18:05:44 +0100

    Max Area of Island

---
+++ NewSwift/Algorithms/AlgorithmsTests/MaxAreaOfIsland.swift
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/max-area-of-island/
+
+import XCTest
+
+final class MaxAreaOfIsland: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(6, maxAreaOfIsland([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]))
+        //XCTAssertEqual(0, maxAreaOfIsland([[0,0,0,0,0,0,0,0]]))
+    }
+    
+    func maxAreaOfIsland(_ grid: [[Int]]) -> Int {
+        
+    }
+}
