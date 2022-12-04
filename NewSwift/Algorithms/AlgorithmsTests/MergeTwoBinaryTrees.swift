e87a59d76f367ee6ada79a98304da30842d6066b
Andrey Panov <panovdev@gmail.com>
2022-12-04 16:49:48 +0100

    Update MergeTwoBinaryTrees.swift

--- NewSwift/Algorithms/AlgorithmsTests/MergeTwoBinaryTrees.swift
+++ NewSwift/Algorithms/AlgorithmsTests/MergeTwoBinaryTrees.swift
@@ -6,6 +6,29 @@ final class MergeTwoBinaryTrees: XCTestCase {
 
     func testExample() throws {
         
+        let node5 = TreeNode(5)
+        let node3 = TreeNode(3, node5, nil)
+        let node2 = TreeNode(2)
+        let root1 = TreeNode(1, node3, node2)
+        
+        let node4 = TreeNode(4)
+        let node1 = TreeNode(1, nil, node4)
+        let node7 = TreeNode(7)
+        let node33 = TreeNode(3, nil, node7)
+        let root2 = TreeNode(2, node1, node33)
+        
+        let mergedTree = mergeTrees(root1, root2)
+        XCTAssertTrue(true)
+    }
+    
+    func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+        guard let root1 = root1 else { return root2 }
+        guard let root2 = root2 else { return root1 }
+        
+        root1.val += root2.val
+        root1.left = mergeTrees(root1.left, root2.left)
+        root1.right = mergeTrees(root1.right, root2.right)
+        return root1
     }
 
 }
